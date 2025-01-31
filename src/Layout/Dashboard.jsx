750c6c7580f7877e6954790f945802d27cc7c5f9
Md Shahien Khan <shawonkn58@gmail.com>
2025-01-31 11:52:11 +0000

    added crud to cart

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,34 +1,42 @@
 import { FaCalendar, FaHome, FaShoppingCart } from "react-icons/fa";
-import { MdReviews } from "react-icons/md";
+import { MdFastfood, MdReviews } from "react-icons/md";
 import { TbBrandBooking } from "react-icons/tb";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
+    const [cart] = useCart();
     return (
-        <div className="flex ">
+        <div className="flex flex-col md:flex-row">
             {/* dashboard side bar */}
-            <div className="w-64 min-h-screen bg-yellow-700">
+            <div className="w-full md:w-64 min-h-screen bg-yellow-600">
                 <ul className="menu p-5">
-                    {/* p4 chilo niche user home chilo just */}
-                    <li>
-                        <NavLink to='/dashboard/userHome'><FaHome></FaHome> User Home part</NavLink>
+                    <li className="text-xl text-white">
+                        <NavLink to='/dashboard/userHome'><FaHome></FaHome> User Home</NavLink>
                     </li>
-                    <li>
-                        <NavLink to='/dashboard/cart'><FaShoppingCart></FaShoppingCart> My Cart</NavLink>
+                    <li className="text-xl text-white">
+                        <NavLink to='/dashboard/cart'><FaShoppingCart></FaShoppingCart> My Cart ({cart.length})</NavLink>
                     </li>
-                    <li>
+                    <li className="text-xl text-white">
                         <NavLink to='/dashboard/reservation'><FaCalendar></FaCalendar> Reservation</NavLink>
                     </li>
-                    <li>
+                    <li className="text-xl text-white">
                         <NavLink to='/dashboard/review'><MdReviews></MdReviews>Add Review</NavLink>
                     </li>
-                    <li>
+                    <li className="text-xl text-white">
                         <NavLink to='/dashboard/review'><TbBrandBooking></TbBrandBooking> My Booking</NavLink>
+                    </li>
+                    <div className="divider"></div>
+                    <li className="text-xl text-white">
+                        <NavLink to='/'><FaHome></FaHome>Home</NavLink>
+                    </li>
+                    <li className="text-xl text-white">
+                        <NavLink to='/order/salad'><MdFastfood></MdFastfood> Menu</NavLink>
                     </li>
                 </ul>
             </div>
             {/* dashboard content */}
-            <div className="flex-1">
+            <div className="flex-1 p-8">
                 <Outlet></Outlet>
             </div>
         </div>
--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,8 +1,95 @@
+import { FaTrashAlt } from "react-icons/fa";
+import useCart from "../../../hooks/useCart";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const Cart = () => {
+    const [cart, refetch] = useCart();
+    // add total price
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+    const axiosSecure = useAxiosSecure();
+
+    // handle delete from cart
+    const handleDelete = id =>{
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            axiosSecure.delete(`/carts/${id}`)
+                .then(res =>{
+                    refetch();
+                    if(res.data.deletedCount > 0){
+                        Swal.fire({
+                          title: "Deleted!",
+                          text: "This food has been deleted from cart.",
+                          icon: "success"
+                        });
+                    }
+                })
+          }
+        });
+    }
     return (
         <div>
-            <h2 className="6xl">My Cart</h2>
+            {/* cart info */}
+            <div className="flex justify-evenly mb-8">
+                <h2 className="text-xl md:text-3xl">items:{cart.length}</h2>
+                <h2 className="text-xl md:text-3xl">items:{totalPrice}</h2>
+                <button className="btn bg-yellow-600 text-white">Pay Now</button>
+            </div>
+            {/* table */}
+            <div className="overflow-x-auto">
+                <table className="table  w-full">
+                    {/* head */}
+                    <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Delete</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, index) =>  <tr key={item._id}>
+                                <th>
+                                    {index + 1}
+                                </th>
+                                <td>
+                                <div className="flex items-center gap-3">
+                                    <div className="avatar">
+                                    <div className="mask mask-squircle h-12 w-12">
+                                        <img
+                                        src={item.image}
+                                        alt="Avatar Tailwind CSS Component" />
+                                    </div>
+                                    </div>
+                                    
+                                </div>
+                                </td>
+                                <td className="font-semibold">
+                                    {item.name}
+                                </td>
+                                <td className="font-semibold">${item.price}</td>
+                                <th>
+                                <button
+                                onClick={()=> handleDelete(item._id)}
+                                className="btn btn-ghost btn-lg">
+                                    <FaTrashAlt className="text-red-600"></FaTrashAlt>
+                                </button>
+                                </th>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+                </div>
         </div>
     );
 };
